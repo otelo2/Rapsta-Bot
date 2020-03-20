6fdd8dab47ff42e0576edee220836f66f04abba0
Otelo2 <josean2009@live.com.mx>
2020-03-20 13:35:02 -0600

    Add suggestion text to a embed

--- bot.js
+++ bot.js
@@ -8,16 +8,16 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
+var client = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+client.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(client.username + ' - (' + client.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `-`
     if (message.substring(0, 1) == '-') {
@@ -28,7 +28,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // -ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
@@ -37,22 +37,58 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'suggest':
                 //bot.channels.get(`690575674544619571`).send('Sugerencia: ' + args);
                 var procMessage = message.replace('-suggest','');
-                bot.sendMessage({
+                client.sendMessage({
                     to: '690575674544619571', //channelID, //ID of the suggestions channel
-                    message: 'Sugerencia: ' + procMessage
+                    "embed": {
+                        "title": "Sugerencia",
+                        "description": `${procMessage}`,
+                        "color": 1503970,
+                        "timestamp": "2020-03-20T19:14:49.939Z",
+                        "footer": {
+                          "text": `${user}`
+                        },
+                        "author": {
+                          "name": "Rapsta Bot",
+                          "url": "https://github.com/otelo2/Rapsta-Bot",
+                          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                        }
+                      }
                 });
+                //TODO: delete the message the user sent, not the bot
                 break;
             //Make the bot say whatever the user wants
             case 'say':
                 var procMessage = message.replace('-say','');
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID, //channelID, //ID of the suggestions channel
                     message: procMessage
                 });
                 break;
+                //TODO: delete the message the user sent, not the bot
+            //Dev command for testing wip things
+            case 'test':
+                var procMessage = message.replace('-test','');
+                client.sendMessage({
+                    to: '690575674544619571', //channelID, //ID of the suggestions channel
+                    "embed": {
+                        "title": "Sugerencia",
+                        "description": `${procMessage}`,
+                        "color": 1503970,
+                        "timestamp": "2020-03-20T19:14:49.939Z",
+                        "footer": {
+                          "text": `${user}`
+                        },
+                        "author": {
+                          "name": "Rapsta Bot",
+                          "url": "https://github.com/otelo2/Rapsta-Bot",
+                          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                        }
+                      }
+                });
+                break;
             //If there is no known command
             default:
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: '¿Qué ladras?'
                 });
