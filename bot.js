97bf54e0fa15587801973adffed8dc467a67bddb
Otelo2 <josean2009@live.com.mx>
2020-03-20 21:11:26 -0600

    add typing flag if i didnt invoke test

--- bot.js
+++ bot.js
@@ -4,136 +4,134 @@ var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
-    colorize: true
+  colorize: true
 });
 logger.level = 'debug';
 // Initialize Discord Bot
 var client = new Discord.Client({
-   token: auth.token,
-   autorun: true
+  token: auth.token,
+  autorun: true
 });
 client.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(client.username + ' - (' + client.id + ')');
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(client.username + ' - (' + client.id + ')');
 });
 client.on('message', function (user, userID, channelID, message, evt) {
-    // It will listen for messages that will start with `-`
-    if (message.substring(0, 1) == '-') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // -ping
-            case 'ping':
-                client.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            //Suggestions
-            case 'suggest':
-                var procMessage = message.replace('-suggest','');
-                client.sendMessage({
-                    to: '690575674544619571', //channelID, //ID of the suggestions channel
-                    "embed": {
-                        "title": "Sugerencia",
-                        "description": `${procMessage}`,
-                        "color": 1503970,
-                        "timestamp": evt.d.timestamp,
-                        "footer": {
-                          "text": `${user}`
-                        },
-                        "author": {
-                          "name": "Rapsta Bot",
-                          "url": "https://github.com/otelo2/Rapsta-Bot",
-                          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                        }
-                      }
-                });
-                // Delete the message the user sent
-                client.deleteMessage({
-                    channelID: channelID,
-                    messageID: evt.d.id,
-                })
-                
-                break;
-            //Make the bot say whatever the user wants
-            case 'say':
-                var procMessage = message.replace('-say','');
-                client.sendMessage({
-                    to: channelID, //Current channel
-                    message: procMessage
-                });
-                //Delete the message the user sent
-                client.deleteMessage({
-                    channelID: channelID,
-                    messageID: evt.d.id,
-                })
-                break;
-            //Dev command for testing wip things
-            case 'test':
-                //Check if its me
-                if( user.localeCompare("Otelo2") == 0)
-                {
-                  client.sendMessage({
-                    to: channelID, //Current channel
-                    message: "Tú eres Otelo, tú me caes bien."
-                });
-                }
-                else
-                {
-                  client.sendMessage({
-                    to: channelID, //Current channel
-                    message: "No sé quien te crees, pero no te voy a hacer caso."
-                });
-                }
-                break;
-            //Show list of available commands
-            case 'help':
-                var procMessage = message.replace('-test','');
-                client.sendMessage({
-                    to: channelID, 
-                    "embed": {
-                        "title": "Comandos",
-                        "description": "Lista de comandos disponibles para Rapsta Bot. Prefijo: `-`",
-                        "color": 913550,
-                        "timestamp": evt.d.timestamp,
-                        "footer": {
-                          "text": `${user}`
-                        },
-                    
-                        "author": {
-                          "name": "Rapsta Bot",
-                          "url": "https://github.com/otelo2/Rapsta-Bot",
-                          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-                        },
-                        
-                        "fields": [
-                          {
-                            "name": "suggest",
-                            "value": "Haz una sugerencia de funciones que añadir a Rapsta Bot."
-                          },
-                          {
-                            "name": "say",
-                            "value": "Rapsta Bot dice lo que escribas después de éste comando."
-                          },
-                          {
-                            "name": "help",
-                            "value": "Muestra la lista de comandos."
-                          }
-                        ]
-                      }
-                });
-                break;
-            //If there is no known command
-            default:
-                client.sendMessage({
-                    to: channelID,
-                    message: 'Qué ladras?'
-                });
-                break;
-         }
-     }
+  // It will listen for messages that will start with `-`
+  if (message.substring(0, 1) == '-') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+      // -ping
+      case 'ping':
+        client.sendMessage({
+          to: channelID,
+          message: 'Pong!'
+        });
+        break;
+        //Suggestions
+      case 'suggest':
+        var procMessage = message.replace('-suggest', '');
+        client.sendMessage({
+          to: '690575674544619571', //channelID, //ID of the suggestions channel
+          "embed": {
+            "title": "Sugerencia",
+            "description": `${procMessage}`,
+            "color": 1503970,
+            "timestamp": evt.d.timestamp,
+            "footer": {
+              "text": `${user}`
+            },
+            "author": {
+              "name": "Rapsta Bot",
+              "url": "https://github.com/otelo2/Rapsta-Bot",
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+            }
+          }
+        });
+        // Delete the message the user sent
+        client.deleteMessage({
+          channelID: channelID,
+          messageID: evt.d.id,
+        })
+
+        break;
+        //Make the bot say whatever the user wants
+      case 'say':
+        var procMessage = message.replace('-say', '');
+        client.sendMessage({
+          to: channelID, //Current channel
+          message: procMessage
+        });
+        //Delete the message the user sent
+        client.deleteMessage({
+          channelID: channelID,
+          messageID: evt.d.id,
+        })
+        break;
+        //Dev command for testing wip things
+      case 'test':
+        //Check if its me
+        if (user.localeCompare("Otelo2") == 0) {
+          client.sendMessage({
+            to: channelID, //Current channel
+            message: "Tú eres Otelo, tú me caes bien."
+            
+          });
+        } else {
+          client.sendMessage({
+            to: channelID, //Current channel
+            message: "No juegues con poderes que aún no puedes controlar.",
+            typing: true
+          });
+        }
+        break;
+        //Show list of available commands
+      case 'help':
+        var procMessage = message.replace('-test', '');
+        client.sendMessage({
+          to: channelID,
+          "embed": {
+            "title": "Comandos",
+            "description": "Lista de comandos disponibles para Rapsta Bot. Prefijo: `-`",
+            "color": 913550,
+            "timestamp": evt.d.timestamp,
+            "footer": {
+              "text": `${user}`
+            },
+
+            "author": {
+              "name": "Rapsta Bot",
+              "url": "https://github.com/otelo2/Rapsta-Bot",
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+            },
+
+            "fields": [{
+                "name": "suggest",
+                "value": "Haz una sugerencia de funciones que añadir a Rapsta Bot."
+              },
+              {
+                "name": "say",
+                "value": "Rapsta Bot dice lo que escribas después de éste comando."
+              },
+              {
+                "name": "help",
+                "value": "Muestra la lista de comandos."
+              }
+            ]
+          }
+        });
+        break;
+        //If there is no known command
+      default:
+        client.sendMessage({
+          to: channelID,
+          message: 'Qué ladras?'
+        });
+        break;
+    }
+  }
 });
