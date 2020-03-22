e9c71f6908b794769c5548a8bcc645fbffe5dd36
Otelo2 <josean2009@live.com.mx>
2020-03-22 00:10:21 -0600

    Scrolling further down the guide

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
+//Standard configuration
+const fs = require('fs');
 const Discord = require('discord.js');
+
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
-const config = require('./config.json');
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.login(token);
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(config.token);
-
 client.on('message', message => {
 	console.log(message.content);
-	if (message.content === '-ping') {
-		// send back "Pong." to the channel the message was sent in
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (message.content.startsWith(`${prefix}ping`)) {
 		message.channel.send('Pong.');
 	}
+	else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+	else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	}
+	else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+	if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	}
+	else if (args[0] === 'foo') {
+		return message.channel.send('bar');
+	}
+
+	message.channel.send(`First argument: ${args[0]}`);
 
 });
-
