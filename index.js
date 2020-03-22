78e73e4058e579dbe57e9d7019fc7c5efdb9437c
Otelo2 <josean2009@live.com.mx>
2020-03-21 23:39:25 -0600

    Base code

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(config.token);
+
+client.on('message', message => {
+	console.log(message.content);
+	if (message.content === '-ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+
+});
+
