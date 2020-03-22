6bd651ae70b14c7316673a617b4f70394221f053
Otelo2 <josean2009@live.com.mx>
2020-03-22 17:52:50 -0600

    Remove unused commands

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Description of the server',
-	execute(message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-};
