ab449483d845cfabe811b0985af312fa025efdac
Otelo2 <josean2009@live.com.mx>
2020-03-22 18:15:28 -0600

    add privileges command

---
+++ commands/priviliges.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'priviliges',
+    description: 'Revisa si el usuario tiene privilegios para comandos avanzados',
+    args: false,
+    aliases: ['priv'],
+    guildOnly: false,
+    execute(message, args) {
+        if (message.author.id === '113787633930469379') { //My discord ID
+            message.reply("Only you are worthy");
+        }
+        else{
+            message.reply("Aún no puedes acceder a ésa función.");
+        }
+
+    },
+};
