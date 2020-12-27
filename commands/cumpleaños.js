74dc9342ea762ec19066b1fcded1433ffdc138fd
Otelo2 <josean2009@live.com.mx>
2020-12-27 17:35:10 -0600

    Store birthdays in a PostgreSQL database

--- commands/cumpleaños.js
+++ commands/cumpleaños.js
@@ -1,9 +1,14 @@
 //TODO: Maybe further check that the first two digits are <=31 and the last two are <=12
 var fs = require('fs');
 var os = require('os');
-path = require('path');
-//Set where the file that stores the id's and dates is located
-let filepath = path.join(__dirname, '..', 'cumples', 'fechas.txt');
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:marcian0@localhost:5432/localdb',
+    ssl: process.env.DATABASE_URL ? true : false
+});
+
+client.connect();
 
 //RE for the date
 var re = /^\d{2}\/\d{2}$/;
@@ -21,12 +26,16 @@ module.exports = {
 
         if (res) {
             message.reply(`Voy a recordar que cumples el ${args}`);
-            console.log(filepath);
-            let contents = `${message.author.id} ${args}${os.EOL}`
-            fs.appendFile(filepath, contents, function(err,file){
-                if (err) throw err;
-                console.log(`Wrote ${contents} to the file in ${filepath}`);
+
+            //Send the data to the DB
+            client.query(`INSERT into CUMPLES values ('${message.author.id}', '${args}');`, (err, res) => {
+                if (err) throw err; 
+                for (let row of res.rows) {
+                    console.log(JSON.stringify(row));
+                }
+                client.end();
             });
+
         } else {
             message.reply('Por favor escribe tu fecha en formato dd/mm (día/mes). Ej: 23/09.');
         }
